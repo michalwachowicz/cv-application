884bdadab1224404339ce3dcaf5490ae3766624c
Michał Wachowicz <wachowiczmichal1@gmail.com>
2022-01-10 13:28:08 +0100

    Move input styles to share them with select component

--- src/components/elements/Input.jsx
+++ src/components/elements/Input.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { Field, Label, LabelRequired } from './InputStyles';
 
 function Input(props) {
   const { label, type, id, required, onFocusLeft } = props;
@@ -31,39 +32,10 @@ Input.defaultProps = {
   required: false,
 };
 
-const Label = styled.label`
-  color: ${({ theme }) => theme.textContent};
-  font-size: 1rem;
-`;
-
-const LabelRequired = styled(Label)`
-  &::after {
-    content: '*';
-    margin-left: 0.25rem;
-    color: #fa5252;
-  }
-`;
-
 const InputField = styled.input`
-  min-height: 3rem;
-  width: 100%;
-  margin-top: 0.5rem;
-  padding: 0.75rem;
-  border: none;
-  border-radius: 1rem;
-  background-color: ${({ theme }) => theme.input};
-  color: ${({ theme }) => theme.textColor};
-  font-size: 1rem;
-  transition: background-color 0.5s ease;
-  -webkit-appearance: none;
-
+  ${Field}
   &::-webkit-date-and-time-value {
     text-align: left;
-  }
-
-  &:hover,
-  &:active {
-    background-color: ${({ theme }) => theme.inputActive};
   }
 `;
 
