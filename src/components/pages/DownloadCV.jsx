bee64d856aa0a88c2ebf0415ce49de0f5411ff30
Michał Wachowicz <wachowiczmichal1@gmail.com>
2022-01-14 16:12:37 +0100

    Complete building DownloadCV page component

--- src/components/pages/DownloadCV.jsx
+++ src/components/pages/DownloadCV.jsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import completeSvg from '../../assets/complete.svg';
+import ButtonContainer from '../containers/ButtonContainer';
+import { SecondaryButton, SuccessButton } from '../elements/buttons/UIButton';
 
-function DownloadCV() {
-  return <div>Download CV</div>;
+function DownloadCV(props) {
+  const { onChangePage, onDownloadCV } = props;
+  return (
+    <div>
+      <Image src={completeSvg} alt="Illustration of two happy people" />
+      <TextContainer>
+        <Title>Hurray! Your CV is ready to download!</Title>
+        <Description>
+          Download your cv and apply for the job. We wish you good luck!
+        </Description>
+      </TextContainer>
+      <ButtonContainer justify="center" direction="column-reverse">
+        <SecondaryButton onClick={() => onChangePage(1)}>Back</SecondaryButton>
+        <SuccessButton onClick={onDownloadCV}>Download CV</SuccessButton>
+      </ButtonContainer>
+    </div>
+  );
 }
+
+DownloadCV.propTypes = {
+  onChangePage: PropTypes.func.isRequired,
+  onDownloadCV: PropTypes.func.isRequired,
+};
+
+const Image = styled.img`
+  display: block;
+  max-width: 360px;
+  width: 100%;
+  margin: 0 auto 2rem;
+`;
+
+const TextContainer = styled.div`
+  max-width: 480px;
+  margin: 0 auto 2rem;
+  text-align: center;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 0.5rem;
+  font-size: 1.5rem;
+  font-weight: 900;
+`;
+
+const Description = styled.p`
+  font-size: 1rem;
+  font-weight: 300;
+`;
 
 export default DownloadCV;
