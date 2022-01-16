8a8a82bef6fda5e4333499abd3348ddc969f946f
Michał Wachowicz <wachowiczmichal1@gmail.com>
2022-01-16 14:16:54 +0100

    Add custom color property to Divider component

--- src/components/elements/Divider.js
+++ src/components/elements/Divider.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Divider = styled.hr`
   border: none;
-  border-top: 2px solid ${({ theme }) => theme.input};
+  border-top: 2px solid ${({ color, theme }) => color || theme.input};
 `;
 
 export default Divider;
