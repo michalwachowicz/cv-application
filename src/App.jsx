b78e3118024d8e6cb365a86088849b89e7f679f6
michalwachowicz <wachowiczmichal1@gmail.com>
2024-11-03 17:05:56 +0100

    Get dark mode value from localstorage on page load

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Container from "./components/layout/Container";
 import Header from "./components/layout/Header";
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
 
+  const darkModeHandler = () => {
+    localStorage.darkMode = !darkMode;
+    setDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+    const darkModeValue = localStorage.darkMode;
+
+    if (darkModeValue === undefined) localStorage.darkMode = true;
+    else setDarkMode(darkModeValue === "true");
+  }, []);
+
   return (
     <Container darkMode={darkMode}>
-      <Header
-        darkMode={darkMode}
-        onThemeSwitch={() => setDarkMode(!darkMode)}
-      />
+      <Header darkMode={darkMode} onThemeSwitch={darkModeHandler} />
     </Container>
   );
 }
