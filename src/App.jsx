771f09ef1a615acc05283e40e91b9a999df2993f
michalwachowicz <wachowiczmichal1@gmail.com>
2024-11-04 13:32:49 +0100

    Create and style navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import React, { useEffect, useState } from "react";
 import Container from "./components/layout/Container";
 import Header from "./components/layout/Header";
+import Navigation from "./components/layout/Navigation";
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
+  const [activePage, setActivePage] = useState(0);
+
+  const pages = [
+    { id: 0, title: "Personal" },
+    { id: 1, title: "Experience" },
+    { id: 2, title: "Download CV" },
+  ];
 
   const darkModeHandler = () => {
     localStorage.darkMode = !darkMode;
     setDarkMode(!darkMode);
+  };
+
+  // TODO: Change active page
+  // eslint-disable-next-line no-unused-vars
+  const pageChangeHandler = (pageId) => {
+    setActivePage(pageId);
   };
 
   useEffect(() => {
@@ -20,6 +34,7 @@ function App() {
   return (
     <Container darkMode={darkMode}>
       <Header darkMode={darkMode} onThemeSwitch={darkModeHandler} />
+      <Navigation activePage={activePage} pages={pages} />
     </Container>
   );
 }
---
+++ src/components/layout/Navigation.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function NavigationItem({ active, title }) {
+  return (
+    <li className={`nav-list-item ${active ? "nav-list-item-active" : ""}`}>
+      <div className="nav-list-item-title">{title}</div>
+    </li>
+  );
+}
+
+export default function Navigation({ activePage, pages }) {
+  return (
+    <nav className="nav">
+      <ol className="nav-list">
+        {pages.map(({ id, title }) => (
+          <NavigationItem key={id} active={activePage === id} title={title} />
+        ))}
+      </ol>
+    </nav>
+  );
+}
